17d56781d1489c75158e9fc663fb4518c592ecff
hjooy <122010676+hjooy@users.noreply.github.com>
2023-10-15 14:19:37 +0900

    Time: 4 ms (79.34%), Space: 44.7 MB (14.41%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -2,8 +2,8 @@ class Solution {
     static ArrayList<Integer>[] graph;
     static Set<Integer> loopless;
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        Set<Integer> on_path = new TreeSet<>();
-        loopless = new TreeSet<>();
+        Set<Integer> on_path = new HashSet<>();
+        loopless = new HashSet<>();
         graph = new ArrayList[numCourses];
         for (int i = 0; i < numCourses; i++) {
             graph[i] = new ArrayList<>();
@@ -17,7 +17,7 @@ class Solution {
         return true;
     }
 
-    public static boolean has_cycle(Set<Integer> on_path, Integer x) {
+    private static boolean has_cycle(Set<Integer> on_path, Integer x) {
         if (on_path.contains(x)) return true;
         if (loopless.contains(x)) return false;
         on_path.add(x);
