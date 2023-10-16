6d81140ff4e15b5ab56d97f611f603970ad3010d
hjooy <122010676+hjooy@users.noreply.github.com>
2023-10-16 10:54:40 +0900

    Time: 3 ms (94.72%), Space: 44.3 MB (37.75%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,9 +1,7 @@
 class Solution {
     static ArrayList<Integer>[] graph;
-    static Set<Integer> loopless;
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        Set<Integer> on_path = new HashSet<>();
-        loopless = new HashSet<>();
+        int[] visited = new int[numCourses];
         graph = new ArrayList[numCourses];
         for (int i = 0; i < numCourses; i++) {
             graph[i] = new ArrayList<>();
@@ -12,20 +10,19 @@ class Solution {
             graph[prerequisite[0]].add(prerequisite[1]);
         }
         for (int i = 0; i < numCourses; i++) {
-            if (has_cycle(on_path, i)) return false;
+            if (has_cycle(visited, i)) return false;
         }
         return true;
     }
 
-    private static boolean has_cycle(Set<Integer> on_path, Integer x) {
-        if (on_path.contains(x)) return true;
-        if (loopless.contains(x)) return false;
-        on_path.add(x);
+    private static boolean has_cycle(int[] visited, Integer x) {
+        if (visited[x] == 1) return true;
+        if (visited[x] == 2) return false;
+        visited[x] = 1;
         for(Integer node : graph[x]) {
-            if (has_cycle(on_path, node)) return true;
+            if (has_cycle(visited, node)) return true;
         }
-        on_path.remove(x);
-        loopless.add(x);
+        visited[x] = 2;
         return false;
     }
 }
