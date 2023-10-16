7cb0dcf5cf68898c8095384ac70f37b7a4010941
hjooy <122010676+hjooy@users.noreply.github.com>
2023-10-16 11:56:59 +0900

    Time: 5 ms (66.28%), Space: 43.6 MB (80.97%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,28 +1,35 @@
 class Solution {
-    static ArrayList<Integer>[] graph;
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        int[] visited = new int[numCourses];
-        graph = new ArrayList[numCourses];
+        int[] indegree = new int[numCourses];
+        ArrayList<Integer> ans = new ArrayList<>();
+        ArrayList<Integer>[] graph = new ArrayList[numCourses];
+
         for (int i = 0; i < numCourses; i++) {
             graph[i] = new ArrayList<>();
         }
-        for (int[] prerequisite : prerequisites) {
-            graph[prerequisite[0]].add(prerequisite[1]);
-        }
-        for (int i = 0; i < numCourses; i++) {
-            if (has_cycle(visited, i)) return false;
-        }
-        return true;
-    }
 
-    private static boolean has_cycle(int[] visited, Integer x) {
-        if (visited[x] == 1) return true;
-        if (visited[x] == 2) return false;
-        visited[x] = 1;
-        for(Integer node : graph[x]) {
-            if (has_cycle(visited, node)) return true;
+        for (int[] prerequisite : prerequisites) {
+            graph[prerequisite[1]].add(prerequisite[0]); // outgoing edge
+            indegree[prerequisite[0]]++;
         }
-        visited[x] = 2;
-        return false;
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) queue.add(i);
+        }
+
+        while (!queue.isEmpty()) {
+            int cur = queue.poll();
+            ans.add(cur);
+            if (graph[cur] != null) {
+                for (int node : graph[cur]) {
+                    indegree[node]--;
+                    if (indegree[node] == 0) queue.add(node);
+                }
+            }
+        }
+
+        return ans.size() == numCourses;
     }
 }
