8ba2858e369e3fee316b1daeda01a3ee8c4ceea0
hjooy <122010676+hjooy@users.noreply.github.com>
2023-10-17 17:28:27 +0900

    Time: 109 ms (16.01%), Space: 18.9 MB (63.06%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,20 +1,19 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         graph = [ [] for i in range(numCourses) ]
-        loopless = set()
-        path = set()
+        visited = [0] * numCourses
+    
         for pair in prerequisites:
-            graph[pair[1]].append(pair[0])
+            graph[pair[1]].append(pair[0])   
 
         def has_cycle(x: int) -> bool:
-            if x in path: return True
-            if x in loopless: return False
-            path.add(x)
+            if visited[x] == 1: return True
+            if visited[x] == 2: return False
+            visited[x] = 1
             for y in graph[x]:
                 if has_cycle(y):
                     return True
-            path.remove(x)
-            loopless.add(x)
+            visited[x] = 2
             return False
 
         for i in range(numCourses):
