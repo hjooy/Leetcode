21e42355fb4b2f5cd272864f10186ce1750a6225
hjooy <122010676+hjooy@users.noreply.github.com>
2023-10-17 18:02:27 +0900

    Time: 93 ms (85.11%), Space: 17.6 MB (92.24%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,23 +1,24 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         graph = [ [] for i in range(numCourses) ]
-        visited = [0] * numCourses
-    
+        indegree = [0] * numCourses
+        ans = []
+
         for pair in prerequisites:
-            graph[pair[1]].append(pair[0])   
+            graph[pair[1]].append(pair[0])
+            indegree[pair[0]] += 1
 
-        def has_cycle(x: int) -> bool:
-            if visited[x] == 1: return True
-            if visited[x] == 2: return False
-            visited[x] = 1
-            for y in graph[x]:
-                if has_cycle(y):
-                    return True
-            visited[x] = 2
-            return False
-
+        queue = deque([])
         for i in range(numCourses):
-            if has_cycle(i):
-                return False
+            if indegree[i] == 0:
+                queue.append(i)
 
-        return True
+        while queue:
+            x = queue.popleft()
+            ans.append(x)
+            for y in graph[x]:
+                indegree[y] -= 1
+                if indegree[y] == 0:
+                    queue.append(y)
+            
+        return len(ans) == numCourses
