46567a9008032c160507070fa426bca7bd0fc241
hjooy <122010676+hjooy@users.noreply.github.com>
2023-10-17 17:22:52 +0900

    Time: 107 ms (20.27%), Space: 19.1 MB (46.49%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -6,19 +6,19 @@ class Solution:
         for pair in prerequisites:
             graph[pair[1]].append(pair[0])
 
-        def has_cycle(x: int, path: set[int]) -> bool:
+        def has_cycle(x: int) -> bool:
             if x in path: return True
             if x in loopless: return False
             path.add(x)
             for y in graph[x]:
-                if has_cycle(y, path):
+                if has_cycle(y):
                     return True
             path.remove(x)
             loopless.add(x)
             return False
 
         for i in range(numCourses):
-            if has_cycle(i, path):
+            if has_cycle(i):
                 return False
 
         return True
