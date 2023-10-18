5096cd44cb90fbbd528df1318b98cf5f67e83a1a
hjooy <122010676+hjooy@users.noreply.github.com>
2023-10-18 23:40:37 +0900

    Time: 61 ms (23.06%), Space: 16.7 MB (73.45%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -2,9 +2,9 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         list = [0] * 26
         for c in s:
-            list[ord(c) - ord('a')] += 1
+            list[ord(c) - 97] += 1
         for c in t:
-            list[ord(c) - ord('a')] -= 1
+            list[ord(c) - 97] -= 1
         for x in list:
             if x is not 0:
                 return False
