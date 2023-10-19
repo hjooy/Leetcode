ca8102f709ebd6273a1c0193e17d0e10e770a72f
hjooy <122010676+hjooy@users.noreply.github.com>
2023-10-19 11:28:29 +0900

    Time: 4 ms (74.45%), Space: 43.8 MB (24.25%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,28 +1,13 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        Map<Integer, Integer> dict = new HashMap<>();
-        int tmp = 0;
-        for (int i = 0; i < s.length(); i++) {
-            tmp = s.charAt(i) - '0';
-            if (dict.containsKey(tmp)) {
-                dict.replace(tmp, dict.get(tmp) + 1);
-            }
-            else {
-                dict.put(tmp, 1); 
-            }
-        }
-        for (int i = 0; i < t.length(); i++) {
-            tmp = t.charAt(i) - '0';
-            if (dict.containsKey(tmp)) {
-                dict.replace(tmp, dict.get(tmp) - 1);
-            }
-            else {
-                dict.put(tmp, -1); 
-            }
-        }
-        for (Integer key : dict.keySet()) {
-            if (dict.get(key) != 0) return false;
-        }
-        return true;
+        if (s.length() != t.length()) return false;
+
+        char[] a = s.toCharArray();
+        char[] b = t.toCharArray();
+        
+        Arrays.sort(a);
+        Arrays.sort(b);
+        
+        return Arrays.equals(a, b);
     }
 }
