4d24a5ba483587571480fc7028092d13f50770ee
hjooy <122010676+hjooy@users.noreply.github.com>
2023-10-20 18:03:22 +0900

    Time: 66 ms (61.61%), Space: 17.5 MB (41.59%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,14 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        ans = []
         dict = {}
         for i in range(len(nums)):          # O(n)
-            complement = target - nums[i]
-            if complement in dict:          # O(1) 
-                ans = [i, dict.get(complement)]
-                break
-            else: dict[nums[i]] = i
-        return ans
+            if target - nums[i] in dict:    # O(1) 
+                return [i, dict[target - nums[i]]]
+            dict[nums[i]] = i
+        return []
 
 # Time complexity: O(n)
 # Space complexity: O(n)
