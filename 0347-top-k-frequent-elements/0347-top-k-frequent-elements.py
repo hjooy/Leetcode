0024ab3e5b9f2d26d1175d2c69edb3f5bf6bb775
hjooy <122010676+hjooy@users.noreply.github.com>
2023-10-21 21:29:14 +0900

    Time: 94 ms (80.49%), Space: 21.1 MB (57.4%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,16 +1,10 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:  
-        ans = []
         map = defaultdict(int)
         for num in nums:
             map[num] += 1
         sorted_map = dict(sorted(map.items(), key=lambda item: item[1], reverse=True))
-        
-        for key in sorted_map.keys():
-            if len(ans) >= k:
-                break
-            ans.append(key)
-        return ans
+        return list(sorted_map.keys())[:k]
         
 # Time complexity: O(n log n)
 # Space complexity: O(n)
