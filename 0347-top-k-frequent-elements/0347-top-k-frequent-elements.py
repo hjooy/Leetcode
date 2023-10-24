2adf684dac5fefc6dba11553842e52da66e8a80f
hjooy <122010676+hjooy@users.noreply.github.com>
2023-10-24 22:44:35 +0900

    Time: 100 ms (52.64%), Space: 20.9 MB (75.77%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,17 +1,14 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         count = Counter(nums)
-        bucket = [[] for _ in range(len(nums) + 1)]
+        heap = []
 
         for num, freq in count.items():
-            bucket[freq].append(num)
-        
+            heapq.heappush(heap, (-freq, num))
+
         ans = []
-        for i in range(len(nums), 0, -1):
-            if bucket[i] is None : continue
-            for num in bucket[i]:
-                if k == 0: break
-                ans.append(num)
-                k -= 1
-        
+        while k > 0:
+            ans.append(heapq.heappop(heap)[1])
+            k -= 1
+            
         return ans
