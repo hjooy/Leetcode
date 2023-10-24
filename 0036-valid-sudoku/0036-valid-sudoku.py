8032f1af15e4217d6c770dbd76da156b07b32214
hjooy <122010676+hjooy@users.noreply.github.com>
2023-10-25 00:12:49 +0900

    Time: 105 ms (39.64%), Space: 16.2 MB (87.07%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -2,9 +2,9 @@ class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         seen = sum(
             ([(row_num, n), (n, col_num), (row_num//3, col_num//3, n)]
-            for row_num, row in enumerate(board)
-                for col_num, n in enumerate(row)
-                    if n != "."), []) 
+            for row_num in range(9) for col_num in range(9)
+            for n in board[row_num][col_num]
+            if n != "."), []) 
             # sum() usage: flattening a list of lists
 
         return len(seen) == len(set(seen))
