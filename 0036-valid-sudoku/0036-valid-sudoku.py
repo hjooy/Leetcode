6bd3d59cb6ac8225db630acb0b0829c660651bab
hjooy <122010676+hjooy@users.noreply.github.com>
2023-10-25 21:44:22 +0900

    Time: 106 ms (34.47%), Space: 16.2 MB (60.38%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,10 +1,9 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         seen = set()
-        return not any(x in seen or
-                        seen.add(x)
+        return 1 == max(list(Counter(x
                         for row_num, row in enumerate(board)
                         for col_num, n in enumerate(row)
                         if n != "."
-                        for x in ((n, row_num), (col_num, n), (row_num//3, col_num//3, n)))
-                        # set.add() always returns None
+                        for x in ((n, row_num), (col_num, n), (row_num//3, col_num//3, n))).values()) + [1])
+                        # .append(1) instead of + [1] does not work. append() returns None in Python
