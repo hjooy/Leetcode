060c1a58c6c21e16398cd48869ecadd4cbbb5624
hjooy <122010676+hjooy@users.noreply.github.com>
2023-11-08 17:58:35 +0900

    Time: 1 ms (100%), Space: 42 MB (69.22%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,9 +1,49 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+        if (s.isEmpty()) return true;
         
-        for (int i = 0, j = s.length() - 1; i < j; i++, j--) {
-            if (s.charAt(i) != s.charAt(j)) return false;
+        int i = 0;
+        int j = s.length() - 1;
+        int iAscii = 0;
+        int jAscii = 0;
+
+        while (i < j) {
+            iAscii = (int)s.charAt(i);
+            while (!(iAscii >= 48 && iAscii <= 57) &&
+                    !(iAscii >= 65 && iAscii <= 90) &&
+                    !(iAscii >= 97 && iAscii <= 122) && i < s.length() - 1) {
+                i++;
+                iAscii = (int)s.charAt(i);
+            }
+            
+            jAscii = (int)s.charAt(j);
+            while (!(jAscii >= 48 && jAscii <= 57) &&
+                    !(jAscii >= 65 && jAscii <= 90) &&
+                    !(jAscii >= 97 && jAscii <= 122) && j > 0) {
+                j--;
+                jAscii = (int)s.charAt(j);
+            }
+            
+            if (iAscii >= 65 && iAscii <= 90) iAscii += 32; 
+            if (jAscii >= 65 && jAscii <= 90) jAscii += 32; 
+
+            if (iAscii >= 48 && iAscii <= 57) {
+                if (jAscii >= 97 && jAscii <= 122) return false;
+                else if (iAscii >= 48 && iAscii <= 57) {
+                    if (iAscii != jAscii) return false;     
+                }
+                else return false;
+            }            
+            else if (iAscii >= 97 && iAscii <= 122) {
+                if (jAscii >= 48 && jAscii <= 57) return false;
+                else if (jAscii >= 97 && jAscii <= 122) {
+                    if (iAscii != jAscii) return false;
+                }
+                else return false;
+            } 
+
+            i++;
+            j--;
         }
 
         return true;
