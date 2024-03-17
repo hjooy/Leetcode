cc05e4ca9a6ee12181b57777434f60f2e95cc4dd
hjooy <122010676+hjooy@users.noreply.github.com>
2024-03-17 13:16:35 +0900

    Time: 4 ms (96.47%) | Memory: 45 MB (28.60%) - LeetSync

---
+++ 155-min-stack/min-stack.java
@@ -0,0 +1,29 @@
+class MinStack {
+    int min;
+    Stack<Integer> stack;
+
+    public MinStack() {
+        min = Integer.MAX_VALUE;
+        stack = new Stack<Integer>();
+    }
+    
+    public void push(int val) {
+        if (val <= min) {
+            stack.push(min);
+            min = val; 
+        }
+        stack.push(val);
+    }
+    
+    public void pop() {
+        if (stack.pop() == min) min = stack.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return min;
+    }
+}
