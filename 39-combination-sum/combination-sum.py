76af5d2d3434b5b093751ccdbcf6e09bb18f206e
hjooy <122010676+hjooy@users.noreply.github.com>
2024-03-20 18:12:43 +0900

    Time: 57 ms (39.05%) | Memory: 16.6 MB (85.55%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,18 +1,18 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         result = []
-        def helper(i: int, sum: int, com: List[int]):
+        n = len(candidates)
+        def helper(idx: int, sum: int, com: List[int]):
             if sum > target: return
             elif sum == target:
                 result.append(com);
                 return;
-            sum += candidates[i]
-            com1 = com[:] + [candidates[i]]
-            helper(i, sum, com1)
-            if i < len(candidates) - 1:
-                sum -= candidates[i]
-                com2 = com1[:-1]
-                helper(i + 1, sum, com2)
-
+            for i in range(idx, n):
+                helper(i, sum + candidates[i], com + [candidates[i]])
+            
         helper(0, 0, []);
         return result;
+
+    # Does list concatenation with the `+` operator always return a new `list` instance?
+    # https://stackoverflow.com/questions/64644671/does-list-concatenation-with-the-operator-always-return-a-new-list-instanc
+    # The reference documentation does not explicitly guarantee that :list + :list results in a new list. However, it is implied by the += augmented assignment being explicitly in-place if possible in contrast to the + operator.
