b300a438e5527aae31e426e5d3e49d36d3194f39
hjooy <122010676+hjooy@users.noreply.github.com>
2024-03-20 18:02:17 +0900

    Time: 56 ms (43.26%) | Memory: 16.8 MB (16.41%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,6 +1,7 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        def helper(i: int, sum: int, com: List[int], result: List[List[int]]):
+        result = []
+        def helper(i: int, sum: int, com: List[int]):
             if sum > target: return
             elif sum == target:
                 result.append(com);
@@ -8,12 +9,11 @@ class Solution:
             sum += candidates[i]
             com1 = com[:]
             com1.append(candidates[i])
-            helper(i, sum, com1, result)
+            helper(i, sum, com1)
             if i < len(candidates) - 1:
                 sum -= candidates[i]
                 com2 = com1[:-1]
-                helper(i + 1, sum, com2, result)
+                helper(i + 1, sum, com2)
 
-        result = []
-        helper(0, 0, [], result);
+        helper(0, 0, []);
         return result;
