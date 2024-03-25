8d5ebfe156dcdc77beef4e9828b87a5e31a7be9b
hjooy <122010676+hjooy@users.noreply.github.com>
2024-03-25 16:56:08 +0900

    Time: 3 ms (86.60%) | Memory: 49.2 MB (77.31%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,15 +1,10 @@
 class Solution {
-    int[][] visited;
-    int[] vec_r = new int[]{0, 0, -1, 1};
-    int[] vec_c = new int[]{-1, 1, 0, 0};
-    int count = 0;
-
     public int numIslands(char[][] grid) {
-        visited = new int[grid.length][grid[0].length];
+        int count = 0;
 
         for (int r = 0; r < grid.length; r++) {
             for (int c = 0; c < grid[0].length; c++) {
-                if (visited[r][c] == 1 || grid[r][c] == '0') continue;
+                if (grid[r][c] == '0') continue;
                 dfs(r, c, grid);
                 count += 1;
             } 
@@ -19,14 +14,11 @@ class Solution {
     }
 
     private void dfs(int r, int c, char[][] grid) {
-        if (visited[r][c] == 1 || grid[r][c] == '0') return;
-        visited[r][c] = 1;
-        for (int i = 0; i < 4; i++) {
-            int new_r = r + vec_r[i];
-            int new_c = c + vec_c[i];
-            if (new_r < 0 || new_r >= grid.length || new_c < 0 || new_c >= grid[0].length) continue;
-            if (visited[new_r][new_c] == 1 || grid[new_r][new_c] == '0') continue;
-            dfs(new_r, new_c, grid);
-        }
+        if (r < 0 || r >= grid.length || c < 0 || c >= grid[0].length || grid[r][c] == '0') return;
+        grid[r][c] = '0';
+        dfs(r - 1, c, grid);
+        dfs(r + 1, c, grid);
+        dfs(r, c - 1, grid);
+        dfs(r, c + 1, grid);
     }
 }
