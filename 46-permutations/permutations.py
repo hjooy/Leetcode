0de5171210908578822749fadc25074803215c57
hjooy <122010676+hjooy@users.noreply.github.com>
2024-03-26 11:49:10 +0900

    Time: 43 ms (41.95%) | Memory: 16.7 MB (36.58%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,14 +1,14 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         output = []
-        def dfs(cur: List[int]):
+        def backtrack(cur: List[int]):
             if len(cur) == len(nums):
                 output.append(cur)
                 return;
             for i, n in enumerate(nums):
                 if n in cur: continue
-                dfs(cur + [n])
+                backtrack(cur + [n])
 
-        dfs([])
+        backtrack([])
 
         return output
