0071177613ae94cd8b0119e3362c1a394268d9e0
hjooy <122010676+hjooy@users.noreply.github.com>
2024-03-26 15:43:18 +0900

    Time: 1 ms (100.00%) | Memory: 44.8 MB (22.59%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.java
+++ 695-max-area-of-island/max-area-of-island.java
@@ -1,32 +1,20 @@
 class Solution {
-    int[] vec_r = {0, 0, -1, 1};
-    int[] vec_c = {1, -1, 0, 0};
-
     public int maxAreaOfIsland(int[][] grid) {
-        int[] maxSize = {0};
-        int[] tmpSize;
+        int ans = 0;
+
         for (int r = 0; r < grid.length; r++) {
             for (int c = 0; c < grid[0].length; c++) {
                 if (grid[r][c] == 0) continue;
-                tmpSize = new int[]{0};
-                dfs(grid, r, c, maxSize, tmpSize);
+                ans = Math.max(ans, dfs(grid, r, c));
             }
         }
             
-        return maxSize[0];
+        return ans;
     }
 
-    private void dfs(int[][] grid, int r, int c, int[] maxSize, int[] curSize) {
-        if (r < 0 || r >= grid.length || c < 0 || c >= grid[0].length || grid[r][c] == 0) {
-            if (maxSize[0] < curSize[0]) maxSize[0] = curSize[0];
-            return;
-        }
-        curSize[0] += 1;
+    private int dfs(int[][] grid, int r, int c) {
+        if (r < 0 || r >= grid.length || c < 0 || c >= grid[0].length || grid[r][c] == 0) return 0;
         grid[r][c] = 0;
-        for (int i = 0; i < 4; i++) {
-            int next_r = r + vec_r[i];
-            int next_c = c + vec_c[i];            
-            dfs(grid, next_r, next_c, maxSize, curSize);
-        }
+        return 1 + dfs(grid, r + 1, c) + dfs(grid, r - 1, c) + dfs(grid, r, c + 1) + dfs(grid, r, c - 1);
     }
 }
