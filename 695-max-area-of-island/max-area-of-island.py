2ce5470b97dc2fb7bd943ac70c03ac75f164ca17
hjooy <122010676+hjooy@users.noreply.github.com>
2024-03-26 15:32:21 +0900

    Time: 102 ms (81.89%) | Memory: 17.1 MB (61.81%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -1,22 +1,10 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        vec = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-
-        def dfs(r:int, c:int, cur:List[int], max:List[int]):
-            if r < 0 or r >= len(grid) or c < 0 or c >= len(grid[0]) or grid[r][c] == 0:
-                max[0] = cur[0] if max[0] < cur[0] else max[0]
-                return
+        def dfs(r:int, c:int) -> int:
+            if r < 0 or r >= len(grid) or c < 0 or c >= len(grid[0]) or grid[r][c] == 0:    
+                return 0
             grid[r][c] = 0
-            cur[0] += 1
-            for v in vec:
-                next_r = r + v[0]
-                next_c = c + v[1]
-                dfs(next_r, next_c, cur, max)
+            return 1 + dfs(r + 1, c) + dfs(r - 1, c) + dfs(r, c + 1) + dfs(r, c - 1)
 
-        max = [0]
-        for r in range(len(grid)):
-            for c in range(len(grid[0])):
-                cur = [0]
-                dfs(r, c, cur, max)
-        
-        return max[0]
+        islands = [ dfs(r, c) for r in range(len(grid)) for c in range(len(grid[0])) if grid[r][c] == 1 ]
+        return max(islands) if islands else 0
