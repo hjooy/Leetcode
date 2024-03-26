8ac689d4534413b8df1f4ca56b2a8ca041370b75
hjooy <122010676+hjooy@users.noreply.github.com>
2024-03-26 11:48:54 +0900

    Time: 2 ms (49.40%) | Memory: 44.5 MB (31.03%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -2,11 +2,11 @@ class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
         List<Integer> cur = new ArrayList<>();
-        dfs(nums, cur, result);
+        backtrack(nums, cur, result);
         return result;
     }
 
-    private void dfs(int[] nums, List<Integer> cur, List<List<Integer>> result) {
+    private void backtrack(int[] nums, List<Integer> cur, List<List<Integer>> result) {
         if (cur.size() == nums.length) {
             result.add(cur);
             return;
@@ -16,7 +16,7 @@ class Solution {
             List<Integer> nextCur = new ArrayList<>();
             nextCur.addAll(cur);
             nextCur.add(n);
-            dfs(nums, nextCur, result);
+            backtrack(nums, nextCur, result);
         }
     }
 }
