5ea55cc686a16963196ba3c011d253a5b143339a
hjooy <122010676+hjooy@users.noreply.github.com>
2024-04-15 17:54:31 +0900

    Time: 37 ms (49.96%) | Memory: 16.5 MB (44.14%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -4,11 +4,11 @@ class Solution:
             return nums[0]
 
         dp1 = nums[0]
-        dp2 = nums[1] if nums[1] > nums[0] else nums[0]
+        dp2 = max(nums[1], nums[0])
 
         for n in nums[2:]:
             tmp = dp2
-            dp2 = dp1 + n if dp1 + n > dp2 else dp2
+            dp2 = max(dp1 + n, dp2)
             dp1 = tmp
 
-        return dp1 if dp1 > dp2 else dp2
+        return max(dp1, dp2)
