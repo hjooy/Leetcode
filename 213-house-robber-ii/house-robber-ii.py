52f786946b0421d8a4e9f9b455ac08a75e4db4ac
hjooy <122010676+hjooy@users.noreply.github.com>
2024-04-15 18:43:20 +0900

    Time: 38 ms (45.94%) | Memory: 16.6 MB (33.76%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -1,28 +1,23 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         if len(nums) == 1: return nums[0]
-        elif len(nums) == 2: return max(nums[0], nums[1])
 
-        ans = 0
+        dp1, dp2, ans = 0, 0, 0
 
         # 1. nums[1:]
-        dp1 = nums[1]
-        dp2 = max(nums[1], nums[2])
-
-        for n in nums[3:]:
+        for n in nums[1:]:
             tmp = dp2
             dp2 = max(dp1 + n, dp2)
             dp1 = tmp
         
-        ans = max(dp1, dp2)
+        ans = dp2
 
         # 2. nums[:-1]
-        dp1 = nums[0]
-        dp2 = max(nums[0], nums[1])
+        dp1, dp2 = 0, 0
         
-        for n in nums[2:-1]:
+        for n in nums[:-1]:
             tmp = dp2
             dp2 = max(dp1 + n, dp2)
             dp1 = tmp
 
-        return max(ans, dp1, dp2)        
+        return max(ans, dp2)        
